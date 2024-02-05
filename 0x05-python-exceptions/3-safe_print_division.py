e443cfed0ca1fb77f51e920fa5349360f340f5ac
wanyonyi-dev <wanyonyip148@gmail.com>
2024-02-05 09:35:53 -0800

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    Qu = 0
+    try:
+        qu = a / b
+    except ZeroDivisionError:
+        qu = None
+    finally:
+        print("Inside result: {}".format(qu))
+    return qu
+
