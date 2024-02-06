4c24216a7b7e01b492e33e3d64f1e7f515846dc9
wanyonyi-dev <wanyonyip148@gmail.com>
2024-02-06 05:04:28 -0800

    100-main.py

---
+++ 0x06-python-classes/100-main.py
@@ -0,0 +1,15 @@
+
+sll = SinglyLinkedList()
+sll.sorted_insert(2)
+sll.sorted_insert(5)
+sll.sorted_insert(3)
+sll.sorted_insert(10)
+sll.sorted_insert(1)
+sll.sorted_insert(-4)
+sll.sorted_insert(-3)
+sll.sorted_insert(4)
+sll.sorted_insert(5)
+sll.sorted_insert(12)
+sll.sorted_insert(3)
+print(sll)
+
