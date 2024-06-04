f3aefeeef43a2245422df2fb1b0494f8c51ab8d8
wanyonyi-dev <wanyonyip148@gmail.com>
2024-06-04 03:55:03 -0700

    task 1

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/**
+ * Read and print the contents of a file.
+ */
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+try {
+  const data = fs.readFileSync(filePath, 'utf-8');
+  console.log(data);
+} catch (err) {
+  console.error(err);
+}
