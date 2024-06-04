ada2326102b4b32e06f7b92b94fae9992944102e
wanyonyi-dev <wanyonyip148@gmail.com>
2024-06-04 05:36:50 -0700

    task 3

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,20 +1,5 @@
 #!/usr/bin/node
-/**
- * This script makes an HTTPS GET request to a specified URL and logs the response code.
- */
-const https = require('https');
-const args = process.argv.slice(2);
-
-https
-  .get(args[0], (res) => {
-    if (res.statusCode === 200) {
-      console.log('code: 200');
-    } else if (res.statusCode === 404) {
-      console.log('code: 404');
-    } else {
-      console.log('code: ' + res.statusCode);
-    }
-  })
-  .on('error', (err) => {
-    console.log('Error: ' + err.message);
-  });
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
